cac7116b894135620a1ef8352c4ef825a718f533
Manasa <raj12ju@gmail.com>
2020-06-07 19:08:29 +0530

    c34

--- sketch.js
+++ sketch.js
@@ -1,29 +1,50 @@
 var ball;
+var datebase;
+var position
 
 function setup(){
+    datebase=firebase.database();
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+   var ballPosition=datebase.ref('ball/position');
+   ballPosition.on("value",readpos,showerror);
 }
 
 function draw(){
     background("white");
+    if(position!==undefined){
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
+
     drawSprites();
 }
+}
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    datebase.ref('ball/position').set({
+        x :position.x + x,
+        y : position.y + y
+    })
+    
+}
+
+function readpos(data){
+    position=data.val();
+    ball.x=position.x;
+    ball.y=position.y;
+}
+
+function showerror(){
+console.log("show my error");
 }

82e477930fab93de723172c5a5030ff2ce51217f
Manasa <raj12ju@gmail.com>
2020-06-09 21:20:51 +0530

    c35

--- sketch.js
+++ sketch.js
@@ -1,50 +1,23 @@
-var ball;
-var datebase;
-var position
+var canvas,backgroundImg;
+var database;
+var playerCount;
+var gameState=0
+var form,player,game;
 
 function setup(){
-    datebase=firebase.database();
-    createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
-   var ballPosition=datebase.ref('ball/position');
-   ballPosition.on("value",readpos,showerror);
+    database=firebase.database();
+    cavas=createCanvas(400,400);
+    game=new Game();
+    game.getState();
+    game.start();
+    
 }
 
 function draw(){
     background("white");
-    if(position!==undefined){
-    if(keyDown(LEFT_ARROW)){
-        writePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        writePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        writePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        writePosition(0,+1);
-    }
+   
 
-    drawSprites();
-}
+   
 }
 
-function writePosition(x,y){
-    datebase.ref('ball/position').set({
-        x :position.x + x,
-        y : position.y + y
-    })
-    
-}
 
-function readpos(data){
-    position=data.val();
-    ball.x=position.x;
-    ball.y=position.y;
-}
-
-function showerror(){
-console.log("show my error");
-}
